48d025f2f05798ed8c55e7695bd53a9ba320db62
GUNGODD <chauhanadityac01@gmail.com>
2024-04-12 12:53:53 +0530

    scripted replaced

--- src/Components/TextArea.jsx
+++ src/Components/TextArea.jsx
@@ -2,19 +2,21 @@ import { useState } from "react"
 
 export const TextArea = () => {
     const [text, setText] = useState(" ");
+
+    const hanldeChange =(e) => {
+        let  newText = e.target.value;
+        setText(newText);
+        if (newText.includes("<script>")){
+        alert("No Script Tag Allowd!");
+        newText = newText.replace('<script>',' ');
+        }
+        setText(newText);
+    }
     return (
         <>
             <textarea
                 placeholder="Enter you text"
-                onChange={(e) => {
-                    let  newText = e.target.value;
-                    setText(newText);
-                    if (newText.includes("<script>")){
-                    alert("No Script Tag Allowd!");
-                    newText = newText.replace('<script>',' ');
-                    }
-                    setText(newText);
-                }}
+                onChange={hanldeChange}
               
                 value={text}
                 className="textarea"
