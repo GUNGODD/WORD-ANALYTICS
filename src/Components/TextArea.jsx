73091706779caf6fc5cb345334b437af9e2546ad
GUNGODD <chauhanadityac01@gmail.com>
2024-04-12 12:52:07 +0530

    event include

--- src/Components/TextArea.jsx
+++ src/Components/TextArea.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react"
 
 export const TextArea = () => {
-  return (
-   <>
-  <textarea 
-  onChange={(e)=>{
-    console.log(e.target.value)
-  } }
-  
-  className="textarea"
-  placeholder="Enter you text"
-  spellCheck="false"
-  
-  />
-   </>
-  )
+    const [text, setText] = useState(" ");
+    return (
+        <>
+            <textarea
+                placeholder="Enter you text"
+                onChange={(e) => {
+                    let  newText = e.target.value;
+                    setText(newText);
+                    if (newText.includes("<script>")){
+                    alert("No Script Tag Allowd!");
+                    newText = newText.replace('<script>',' ');
+                    }
+                    setText(newText);
+                }}
+              
+                value={text}
+                className="textarea"
+
+                spellCheck="false"
+
+            />
+        </>
+    )
 }
 
