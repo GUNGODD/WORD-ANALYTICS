c4c2c38ae2341bdc0c9b8c92e53dc96ac26de790
GUNGODD <chauhanadityac01@gmail.com>
2024-04-13 01:11:35 +0530

    arrow functions has been fixed

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import { Stats } from "./Stats"
 import { TextArea } from "./TextArea"
-const INSTAGRAM_MAX_CHARACTERS = 280;
-const X_MAX_CHARACTERS =50;
+
  const Container = () => {
     const [text, setText] = useState("");
     
     const stats  = {
       numberOfWords:text.split(/\s/).filter((word)=> word !=="").length,
       numberOfCharacters:text.length,
-      InstagramCharacterLeft:INSTAGRAM_MAX_CHARACTERS- text.length,
-      XCharacterLeft:X_MAX_CHARACTERS- text.length,
+      InstagramCharacterLeft:200- text.length,
+      XCharacterLeft:80- text.length,
     }
   return (
 <main className="container">
