18eb543f6610ccf770570f78a081b880e8c7f6cf
GUNGODD <chauhanadityac01@gmail.com>
2024-04-12 18:06:36 +0530

    props fixed

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,31 +1,19 @@
-
 export const Stats = () => {
   return (
-  <section className="stats">
-
-<Stat stat_number={0} platform={"Words"} />
-<Stat stat_number={0} platform={"CHARACTER"} />
-<Stat stat_number={2200} platform={"INSTAGRAM"} />
-<Stat stat_number={0} platform={"X"} />
-
-
-
-
-
-  </section>
+    <section className="stats">
+      <Stat stat_number={0} platform={"Words"} />
+      <Stat stat_number={0} platform={"CHARACTER"} />
+      <Stat stat_number={2200} platform={"INSTAGRAM"} />
+      <Stat stat_number={0} platform={"X"} />
+    </section>
   );
-}
+};
 
-
-function Stat({stat_number, platform}){
-    return (
-        <section className="stat">
-    <span className="stat__number"> {stat_number}</span>
-    <h2 className="second-heading">{platform}</h2>
-</section>
-
-    )
-
-
-
+function Stat({ stat_number, platform }) {
+  return (
+    <section className="stat">
+      <span className="stat__number"> {stat_number}</span>
+      <h2 className="second-heading">{platform}</h2>
+    </section>
+  );
 }
