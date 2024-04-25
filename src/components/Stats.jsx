3f59e79ec00cd1a0e567323bf802e084f211a686
GUNGODD <chauhanadityac01@gmail.com>
2024-04-25 21:20:47 +0530

    [test]

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -7,6 +7,15 @@ export default function Stats({ stats }) {
       <Stat number={stats.numberOfCharacters} type="Characters" />
       <Stat number={stats.instagramCharactersLeft} type="Instagram" />
       <Stat number={stats.facebookCharactersLeft} type="Facebook" />
+      <div className="stat">
+        <button className="border  m-4 p-3 hover:border-red-500 bg-blue-500 hover:bg-blue-300 rounded-lg">
+          {" "}
+          Sumbit{" "}
+        </button>
+      </div>
+      <div className="stat">
+        <h2>final answer </h2>
+      </div>
     </section>
   );
 }
