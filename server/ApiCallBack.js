dd538b95ecb20559397f43e8b6cc12417b2bb820
GUNGODD <chauhanadityac01@gmail.com>
2024-05-03 16:39:01 +0530

    changes

--- server/ApiCallBack.js
+++ server/ApiCallBack.js
@@ -1,9 +1,14 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(
   " AIzaSyBNXGIMacH2OKQa_7qJ0FjybzDz_AAIgrQ",
 );
+
+
+
+
+
+
 
 async function run() {
   // For text-only input, use the gemini-pro model
@@ -16,8 +21,11 @@ async function run() {
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
-  const text = response.text();
+    const  text = response.text();
   console.log(text);
+
 }
 
 run();
+
+
